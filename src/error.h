6381362b2bd32613ef8d0fc6a056bbb264674da9
Mateus Marques <mateusmarques2001@usp.br>
2026-05-14 22:13:28 -0300

    Read first BIOS instruction

---
+++ src/error.h
@@ -0,0 +1,10 @@
+#ifndef _ERROR_H
+#define _ERROR_H
+
+#include <stdarg.h>
+
+#define	MAXLINE	4096			/* max line length */
+
+void err_quit(const char *fmt, ...);
+
+#endif // _ERROR_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,49 @@
+#include "error.h"
+
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+uint8_t bios[512 * 1024];
+
+bool read_bios(const char *filename)
+{
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) return false;
+
+    fseek(fp, 0L, SEEK_END);
+    long size = ftell(fp);
+    rewind(fp);
+
+    fread(bios, 1, size, fp);
+
+    fclose(fp);
+
+    return true;
+}
+
+uint32_t load32(uint32_t offset)
+{
+    if ((offset & 0xf) != 0) {
+        err_quit("Unaligned memory access: 0x%08x", offset);
+    }
+    uint8_t a = bios[offset + 0];
+    uint8_t b = bios[offset + 1];
+    uint8_t c = bios[offset + 2];
+    uint8_t d = bios[offset + 3];
+
+    // 0x12345678 in memory -> 0x78563412
+
+    return (d << 24) | (c << 16) | (b << 8) | a;
+}
 
 int main(void)
 {
-    printf("Hello PSX\n");
+    if (!read_bios("SCPH1001.BIN")) {
+        return 1;
+    }
+
+    printf("First BIOS instruction: 0x%08x\n", load32(0));
+
     return 0;
 }
